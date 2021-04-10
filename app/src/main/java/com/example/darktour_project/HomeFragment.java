4b1bab26243d49121748f0d943f9036242f0ced9
BaekDaEun <kks06252@gmail.com>
2021-04-10 09:11:37 +0900

    홈화면 viewpager 추가, 조금씩 수정 예정

--- app/src/main/java/com/example/darktour_project/HomeFragment.java
+++ app/src/main/java/com/example/darktour_project/HomeFragment.java
@@ -1,22 +1,78 @@
 package com.example.darktour_project;
-
+// 메인홈화면
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class HomeFragment extends Fragment {
 
-    @Nullable
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+    View v;
+    //private HomeImageAdapter adapter;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ArrayList<Integer> listImage = new ArrayList<>();
+        listImage.add(R.drawable.busan);
+        listImage.add(R.drawable.seoul);
+        listImage.add(R.drawable.jeju);
+
+        ViewPager viewPager = v.findViewById(R.id.mainhome_viewpager);
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getFragmentManager());
+        // ViewPager와  FragmentAdapter 연결
+        viewPager.setAdapter(fragmentAdapter);
+
+        // FragmentAdapter에 Fragment 추가, Image 개수만큼 추가
+        for (int i = 0; i < listImage.size(); i++) {
+            HomeImageFragment imageFragment = new HomeImageFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("imgRes", listImage.get(i));
+            imageFragment.setArguments(bundle);
+            fragmentAdapter.addItem(imageFragment);
+        }
+        fragmentAdapter.notifyDataSetChanged();
+
+        return v;
     }
+
+    class FragmentAdapter extends FragmentPagerAdapter {
+
+        // ViewPager에 들어갈 Fragment들을 담을 리스트
+        private ArrayList<Fragment> fragments = new ArrayList<>();
+
+        // 필수 생성자
+        FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        // List에 Fragment를 담을 함수
+        void addItem(Fragment fragment) {
+            fragments.add(fragment);
+        }
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
