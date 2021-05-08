68e1cec31f10c341b6642eb851812c90a7bfc09a
hyeju0797 <gpwn0797@naver.com>
2021-05-09 00:34:54 +0900

    아이디 중복체크ㅠ

--- app/src/main/java/com/example/darktour_project/Signup.java
+++ app/src/main/java/com/example/darktour_project/Signup.java
@@ -1,9 +1,14 @@
 package com.example.darktour_project;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -16,6 +21,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Signup extends AppCompatActivity {
     private EditText signupname;
@@ -31,7 +43,9 @@ public class Signup extends AppCompatActivity {
     public static int check_for_register = 0;
     public static Context mContext;
     private boolean check = true;
-    public static int reduplication = 0;
+    public int reduplication = 0;
+    private String TAG = "여기는 signup";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +68,10 @@ public class Signup extends AppCompatActivity {
         signupbt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                CheckEmail checkemail = new CheckEmail();
+                String IP_ADDRESS = "113.198.236.105";
+                checkemail.execute("http://" + IP_ADDRESS + "/check_email.php", signupname.getText().toString(), signupemail.getText().toString(), signuppw.getText().toString());
+
                 // 이름을 입력하지 입력하지 않은 경우
                 if (signupname.getText().toString().length() == 0) {
                     name_eroor.setText("이름을 입력하세요");
@@ -122,21 +140,26 @@ public class Signup extends AppCompatActivity {
                     confirmpw.setBackgroundResource(R.drawable.input_rectangle);
                 }*/
 
+                /*
+
+                Log.d(TAG, "reduplication : " + reduplication);
+
                 if(reduplication>0){
                     Toast.makeText(mContext, "존재하는 이메일입니다.", Toast.LENGTH_LONG).show();
                     Intent intent = new Intent(getApplicationContext(), Signup.class);
                     startActivity(intent);
 
                 } else {
+                    Log.d(TAG, "reduplication : " + reduplication);
+
                     Intent intent = new Intent(getApplicationContext(), Interest.class);
                     startActivity(intent);
                 }
 
+                 */
+
                 //약관동의 안 먹힘.
                 if (check_for_register <= 0 ) {
-                    CheckEmail checkemail = new CheckEmail();
-                    String IP_ADDRESS = "113.198.236.105";
-                    checkemail.execute("http://" + IP_ADDRESS + "/check_email.php", signupname.getText().toString(), signupemail.getText().toString(), signuppw.getText().toString());
 
                 }
 
@@ -199,7 +222,130 @@ public class Signup extends AppCompatActivity {
 
     }
 
-    public static void getcode(int red){
+    public void getcode(int red){
         reduplication = red;
+    }
+
+
+    public class CheckEmail extends AsyncTask<String, Void, String> {
+        private String TAG = "checkemail";
+        String errorString = null;
+        ProgressDialog progressDialog;
+        public int redd;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(Signup.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            Log.d("result : ", result);
+        }
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected String doInBackground(String... params) {
+            String serverURL = (String)params[0];
+            String NAME = (String)params[1];
+            String USER_ID= (String)params[2];
+            String USER_PWD= (String)params[3];
+
+            String postParameters = "email=" + USER_ID;
+
+            Log.d("checkemail : ", postParameters);
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                Log.d("sb : ", sb.toString().trim());
+                if(!sb.toString().trim().contains(USER_ID)){
+                    InsertUserData insertdata = new InsertUserData();
+                    String IP_ADDRESS = "113.198.236.105";
+
+                    insertdata.execute("http://" + IP_ADDRESS + "/register.php", NAME, USER_ID, USER_PWD);
+
+                    Log.d("insert name - ", NAME);
+                    Log.d("insert email - ", USER_ID);
+                    Log.d("insert pwd - ", USER_PWD);
+                    redd=0;
+
+                }else{
+                    Log.d(TAG, "existing email : " + USER_ID);
+                    redd=1;
+                    Log.d(TAG, "existing email  + red: " + USER_ID+redd);
+                    Log.d(TAG, "reduplication : " + reduplication);
+
+
+                    Handler mHandler = new Handler(Looper.getMainLooper());
+                    mHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // 사용하고자 하는 코드
+                            Toast.makeText(Signup.this, "존재하는 이메일입니다.", Toast.LENGTH_LONG).show();
+                        }
+                    }, 0);
+
+                    Intent intent = new Intent(getApplicationContext(), Signup.class);
+                    startActivity(intent);
+
+                }
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "checkemail: Error ", e);
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+
+
     }
 }
