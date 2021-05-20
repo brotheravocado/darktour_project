b79567ab56a3aec6bc57255ba5c81cbfb385a51f
sunhee99 <sunhily99@gmail.com>
2021-05-20 22:00:25 +0900

    관심유적지 완성

---
+++ app/src/main/java/com/example/darktour_project/ProfileAdapter.java
@@ -0,0 +1,69 @@
+package com.example.darktour_project;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
+
+    private ArrayList<Profile> items = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ProfileAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.interestsite_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProfileAdapter.ViewHolder viewHolder, int position) {
+
+        Profile item = items.get(position);
+
+        Glide.with(viewHolder.itemView.getContext()).load(item.getUrl()).into(viewHolder.historyImg);
+
+        viewHolder.historyTitle.setText(item.getTitle());
+        viewHolder.historyContent.setText(item.getContent());
+        viewHolder.historyLikecount.setText(item.getLikecount());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void setItems(ArrayList<Profile> items) {
+        this.items = items;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView historyTitle, historyContent, historyLikecount;
+        ImageView historyImg;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            historyImg = itemView.findViewById(R.id.historic_img);
+
+            historyTitle = itemView.findViewById(R.id.historic_name);
+            historyContent = itemView.findViewById(R.id.historic_desc);
+            historyLikecount = itemView.findViewById(R.id.likes_count);
+        }
+    }
+}
--- app/src/main/java/com/example/darktour_project/ProfileFragment.java
+++ app/src/main/java/com/example/darktour_project/ProfileFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,14 +17,17 @@ import android.widget.ImageButton;
 
 public class ProfileFragment extends Fragment {
     View v;
+    View v1;
     ImageButton setting;
+    private LinearLayoutManager mLayoutManger;
+
+    private ProfileAdapter adapter = new ProfileAdapter();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         v = inflater.inflate(R.layout.fragment_profile, container, false);
-
         setting = v.findViewById(R.id.imageButton);
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +37,26 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        super.onCreate(savedInstanceState);
+
+        //recycleView 초기화
+        RecyclerView recyclerView = v.findViewById(R.id.recycler_view);
+
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        mLayoutManger=new LinearLayoutManager(v.getContext());
+        mLayoutManger.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        recyclerView.setLayoutManager(mLayoutManger);
+
+        recyclerView.setAdapter(adapter);
+
+        //아이템 로드
+        adapter.setItems(new ProfileSampleData().getItems());
         return v;
+
+
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
