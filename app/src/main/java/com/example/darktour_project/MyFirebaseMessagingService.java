68af207e427647650dcb2f57e8f338c16b14100a
hyeju0797 <gpwn0797@naver.com>
2021-05-28 10:33:48 +0900

    공지사항 fcm...3

--- app/src/main/java/com/example/darktour_project/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/darktour_project/MyFirebaseMessagingService.java
@@ -6,10 +6,10 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 
+import androidx.core.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import androidx.core.app.NotificationCompat;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
