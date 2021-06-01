3db6c1e83b7f5aacb92f3576237f0b9d14e34254
BaekDaEun <kks06252@gmail.com>
2021-06-01 19:46:13 +0900

    메인홈화면 이미지 누르면 디테일페이지 이동 완료

--- app/src/main/java/com/travel/darktour_project/HomeImageFragment.java
+++ app/src/main/java/com/travel/darktour_project/HomeImageFragment.java
@@ -36,31 +36,15 @@ public class HomeImageFragment extends Fragment {
             textView.setText(args.getString("nameRes"));
         }
 
-        /*imageView.setOnClickListener(new View.OnClickListener() {
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent;
-                switch (args.getString("imgRes")) {
-                    case R.drawable.busan(): // 클릭한 이미지가 부산이라면
-                        Log.d("지역", "부산");
-                        intent = new Intent(getActivity(), Intro.class); // 넘길 화면 없어서 intro 화면으로 일단 넘김
-                        //intent.putExtra("historyname",homeFragment.bundle.); // 코스이름 DetailPage로 넘김
-                        startActivity(intent);
-                        break;
-                    case (R.drawable.seoul):
-                        Log.d("지역", "서울");
-                        intent = new Intent(getActivity(), Intro.class);
-                        startActivity(intent);
-                        break;
-                    case (R.drawable.jeju):
-                        Log.d("지역", "제주");
-                        intent = new Intent(getActivity(), Intro.class);
-                        startActivity(intent);
-                        break;
-
-                }
+                Log.d("Debug", args.getString("nameRes"));
+                Intent intent = new Intent(getActivity(), DetailPage.class);
+                intent.putExtra("historyname",args.getString("nameRes")); // 코스이름 DetailPage로 넘김
+                startActivity(intent);
             }
-        });*/
+        });
         return view;
     }
 }
