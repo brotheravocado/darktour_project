de1ebc7e0c4fb333e0a1e1bf5e17710216a98a5e
hyeju0797 <gpwn0797@naver.com>
2021-06-09 21:52:44 +0900

    리뷰페이지 수정

--- app/src/main/java/com/travel/darktour_project/ReviewRecyclerAdapter.java
+++ app/src/main/java/com/travel/darktour_project/ReviewRecyclerAdapter.java
@@ -367,28 +367,28 @@ public class ReviewRecyclerAdapter extends RecyclerView.Adapter<ReviewRecyclerAd
                     String line;
                     while((line = bufferedReader.readLine()) != null){
                         sb.append(line);
-                        String text = "/select.php";
-                        if(serverURL.contains(text)) {
-                            //data.setThumb_image(R.drawable.thumbs_up);// 따봉
-                            chk = sb.toString().contains(reviewnum);
-                            if(sb.toString().contains(reviewnum)){
-                                listData.get(getAdapterPosition()).setThumb_image(R.drawable.press_thumbs_up);
-                                listData.get(getAdapterPosition()).setPress(true);
-
-                            }
-                            else{
-                                listData.get(getAdapterPosition()).setThumb_image(R.drawable.thumbs_up);
-                                listData.get(getAdapterPosition()).setPress(false);
-
-                            }
-
-                            //notifyItemChanged(getAdapterPosition());
-
-                            //thumb_button.setImageResource(R.drawable.press_thumbs_up); // 따봉 이미지
-
-                        }
                     }
                     bufferedReader.close();
+                    String text = "/select.php";
+                    if(serverURL.contains(text)) {
+                        //data.setThumb_image(R.drawable.thumbs_up);// 따봉
+                        chk = sb.toString().contains(reviewnum);
+                        if(sb.toString().contains(reviewnum)){
+                            listData.get(getAdapterPosition()).setThumb_image(R.drawable.press_thumbs_up);
+                            listData.get(getAdapterPosition()).setPress(true);
+
+                        }
+                        else{
+                            listData.get(getAdapterPosition()).setThumb_image(R.drawable.thumbs_up);
+                            listData.get(getAdapterPosition()).setPress(false);
+
+                        }
+
+                        notifyItemChanged(getAdapterPosition());
+
+                        //thumb_button.setImageResource(R.drawable.press_thumbs_up); // 따봉 이미지
+
+                    }
                     //String text = "/select.php";
                     //if(serverURL.contains(text)) {
                     //    chk = sb.toString().contains(his_name);
